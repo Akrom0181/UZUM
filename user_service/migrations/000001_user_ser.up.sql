8f6c41e4ba70ed336dd75865b5fda7227ec4c8e5
Akrom <akrom@gmail.com>
2024-06-14 17:16:10 +0500

    user

---
+++ user_service/migrations/000001_user_ser.up.sql
@@ -0,0 +1,67 @@
+CREATE TABLE IF NOT EXISTS customer (
+    id UUID PRIMARY KEY,
+    firstname VARCHAR(55) NOT NUll,
+    lastname VARCHAR(55),
+    phone VARCHAR(20) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    language VARCHAR(2) CHECK (language IN ('uz', 'ru', 'en')) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    gender VARCHAR(6) CHECK (gender IN ('male', 'female')) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    deleted_at INTEGER DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS system_user (
+    id UUID PRIMARY KEY,
+    phone VARCHAR NOT NULL,
+    gmail VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    role VARCHAR(7) CHECK (role IN ('admin', 'courier')) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    deleted_at INTEGER DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS seller (
+    id UUID PRIMARY KEY,
+    phone VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    shop_id UUID REFERENCES shop(id),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    deleted_at INTEGER DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS branch (
+    id UUID PRIMARY KEY,
+    phone VARCHAR NOT NULL,
+    name VARCHAR(20) DEFAULT '',
+    location POLYGON NOT NULL,
+    address VARCHAR NOT NULL,
+    open_time TIME NOT NULL,
+    close_time TIME NOT NULL,
+    active BOOLEAN NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    deleted_at INTEGER DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS shop (
+    id UUID PRIMARY KEY,
+    slug VARCHAR NOT NULL,
+    phone VARCHAR NOT NULL,
+    name_uz VARCHAR(20) DEFAULT '',
+    name_ru VARCHAR(20) DEFAULT '',
+    name_en VARCHAR(20) DEFAULT '',
+    description_uz VARCHAR(500) DEFAULT '',
+    description_ru VARCHAR(500) DEFAULT '',
+    description_en VARCHAR(500) DEFAULT '',
+    location VARCHAR NOT NULL,
+    currency VARCHAR(3) CHECK (currency IN ('USD', 'EUR', 'UZS')) NOT NULL,
+    payment_types VARCHAR[] NOT NULL, -- array note
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    deleted_at INTEGER DEFAULT 0
+);
